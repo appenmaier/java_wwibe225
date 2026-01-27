ea48548b0ebab28273b326b8e83a28d02ed794e4
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2026-01-27 12:49:58 +0100

    Add HelloWorld-Demo

---
+++ src/main/java/main/D01_HelloWorld.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Hello World
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class D01_HelloWorld {
+
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+   }
+
+}
