fee1535785b09adfe0b3ac2ed349b3c27712beab
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2026-02-03 10:01:15 +0100

    Update Demo 1

--- src/main/java/main/D01_HelloWorld.java
+++ src/main/java/main/D01_HelloWorld.java
@@ -10,7 +10,12 @@ package main;
 public class D01_HelloWorld {
 
    public static void main(String[] args) {
-      System.out.println("Hello World");
+      String text; // Deklaration
+
+      text = "Hello World"; // Initialisierung
+
+      /* Ausgabe */
+      System.out.println(text);
    }
 
 }
