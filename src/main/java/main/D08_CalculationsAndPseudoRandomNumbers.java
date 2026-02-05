ef72dc1b5c0d71cb987dd7095d6b627b2546c8bd
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2026-02-05 12:14:16 +0100

    Add Demo 8 (Calculations and Pseudo Random Numbers) and Demo 9 (Console Inputs)

---
+++ src/main/java/main/D08_CalculationsAndPseudoRandomNumbers.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Random;
+
+/**
+ * Calculations and Pseudo Random Numbers
+ *
+ * @author Daniel Appenmaier
+ * @version 1.0
+ *
+ */
+public class D08_CalculationsAndPseudoRandomNumbers {
+
+   public static void main(String[] args) {
+      /* Mathematische Berechnungen */
+      double result = Math.sqrt(64);
+
+      System.out.println("result: " + result);
+
+      /* Pseudozufallszahlen */
+      int randomNumber;
+
+      randomNumber = (int) (Math.random() * 100) + 1;
+      System.out.println("randomNumber: " + randomNumber);
+
+      Random random = new Random();
+      randomNumber = random.nextInt(1, 101);
+      System.out.println("randomNumber: " + randomNumber);
+   }
+
+}
