a3a1ad8d1c6b5afa3e71207e9bc98267d89e14ea
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2026-02-05 16:34:45 +0100

    Update Scoreboard

--- src/main/java/main/D10_ConsoleOutputs.java
+++ src/main/java/main/D10_ConsoleOutputs.java
@@ -46,12 +46,13 @@ public class D10_ConsoleOutputs {
 
       System.out.println();
 
-      System.out.printf("%-10s %s%n", "Name", "Note");
-      System.out.println("---------------");
-      System.out.printf("%-10s %.1f%n", "Hans", 1.5);
-      System.out.printf("%-10s %.1f%n", "Peter", 4.37);
-      System.out.printf("%-10s %.1f%n", "Lisa", 2.3);
-      System.out.printf("%-10s %.1f%n", "Heidi", 2.25);
+      // Beispiel: Notentabelle
+      System.out.printf("| %-7s | %-10s | %-7s |%n", "ID", "Name", "Prozent");
+      System.out.println("| ------- | ---------- | ------- |");
+      System.out.printf("| %-7s | %-10s | %-7.1f |%n", "8172645", "Hans", 95.341);
+      System.out.printf("| %-7s | %-10s | %-7.1f |%n", "1203653", "Peter", 40.716);
+      System.out.printf("| %-7s | %-10s | %-7.1f |%n", "2912834", "Lisa", 28.827);
+      System.out.printf("| %-7s | %-10s | %-7.1f |%n", "3982716", "Heidi", 88.359);
    }
 
 }
